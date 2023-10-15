317390529908f51202cdbf565fdf132dbc266dfa
lanchox <luca.paradisi@uniacc.edu>
2023-10-15 14:31:55 -0300

    v0.0.8
    Añadiendo Drag&Drop

--- Assets/01_Scripts/Kid.cs
+++ Assets/01_Scripts/Kid.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,12 +6,12 @@ using UnityEngine;
 public class Kid : MonoBehaviour
 {
     Rigidbody rb;
-    bool detectarMouse;
     //Tipo de distracción u obstáculo.
     [SerializeField] Transform distraction;
     //Está o no distraído, y si tocó el trigger de la distracción o no.
-    [SerializeField]bool distracted, target;
-    [SerializeField] float speed;
+    [SerializeField]bool distracted, target, pressed;
+    [SerializeField] float speed, pickVelocity;
+    private Vector3 cam;
     void Awake()
     {
         rb = GetComponent<Rigidbody>();
@@ -18,7 +19,7 @@ public class Kid : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(distracted)
+        if(distracted && !pressed)
         {
             if(!target)
             {
@@ -30,28 +31,58 @@ public class Kid : MonoBehaviour
                 rb.velocity = new Vector3(0f, rb.velocity.y, 0f);
             }
         }
-        else
+        else if(!pressed)
         {
-            Vector3 finalVelocity = transform.parent.position - new Vector3(transform.position.x, -rb.velocity.y, transform.position.z);
+            var p = transform.position;
+            Vector3 finalVelocity = transform.parent.position - new Vector3(p.x, -rb.velocity.y, p.z);
             rb.velocity = new Vector3(finalVelocity.x * speed, rb.velocity.y, finalVelocity.z * speed);
         }
+        else if (pressed)
+        {
+            rb.velocity = Vector3.zero;
+        }
         RotateTowards();
+        Touched();
     }
     void RotateTowards()
     {
-        if(distracted && rb.velocity != Vector3.zero)
+        var p = transform.parent.position;
+        if(distracted && rb.velocity != Vector3.zero && !pressed)
         {
             transform.LookAt(new Vector3(distraction.position.x, transform.position.y, distraction.position.z));
         }
-        else if(rb.velocity != Vector3.zero)
+        else if(rb.velocity != Vector3.zero && !pressed)
         {
-            transform.LookAt(transform.parent.position + new Vector3(0f,transform.position.y, 0f));
+            transform.LookAt(new Vector3(p.x,transform.position.y, p.z));
         }
     }
-    private void OnTriggerEnter()
+
+    public void OnMouseDown()
+    {
+        pressed = true;
+    }
+
+    public void OnMouseUp()
+    {
+        pressed = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
     {
         /*Iniciar animaciones
         y eventos cuando el niño 
         interactúe con la distracción*/
+    }
+
+    void Touched()
+    {
+        if (pressed)
+        {
+            cam = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            float currentPos = transform.position.y;
+            float offSet = cam.y - currentPos;
+            Vector3 targetPosition = new Vector3(cam.x, offSet, cam.z);
+            transform.position = targetPosition;
+        }
     }
 }
