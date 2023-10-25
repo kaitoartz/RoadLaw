243feeb3a6003b9782358bed622b04f603c9b3fe
KaitoOwO <kaitoartz@hotmail.com>
2023-10-25 02:02:02 -0300

    v1.9.5
    Semaforos listos

--- Assets/01_Scripts/Semaforo.cs
+++ Assets/01_Scripts/Semaforo.cs
@@ -2,60 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum TrafficLightState
-{
-    Red,
-    Yellow,
-    Green
-}
-
 public class Semaforo : MonoBehaviour
-{ 
-    public delegate void TrafficLightChanged(TrafficLightState newState);
-    public static event TrafficLightChanged OnTrafficLightChanged;
-    public TrafficLightState currentState;
-    public float redTime = 10f;
-    public float yellowTime = 3f;
-    public float greenTime = 15f;
-
-    void Start()
-    {
-        StartCoroutine(TrafficLightSequence());
-    }
-
-    void Update()
-    {
-        switch (currentState)
-        {
-            case TrafficLightState.Red:
-                // Lógica para estado rojo 
-                break;
-
-            case TrafficLightState.Yellow:
-                // Lógica para estado amarillo
-                break;
-
-            case TrafficLightState.Green:
-                // Lógica para estado verde
-                break;
-        }
-    }
-
-    IEnumerator TrafficLightSequence()
-    {
-        while (true)
-        {
-            currentState = TrafficLightState.Red;
-            OnTrafficLightChanged?.Invoke(currentState);
-            yield return new WaitForSeconds(redTime);
-
-            currentState = TrafficLightState.Green;
-            OnTrafficLightChanged?.Invoke(currentState);
-            yield return new WaitForSeconds(greenTime);
-
-            currentState = TrafficLightState.Yellow;
-            OnTrafficLightChanged?.Invoke(currentState);
-            yield return new WaitForSeconds(yellowTime);
-        }
-     }
+{
+    public bool isActive = true;
 }
