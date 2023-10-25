9bb5c74cc393da73d164ba80951688192dd067f7
JuaNavarr <Juan.navarretes@uniacc.edu>
2023-10-24 22:41:27 -0300

    2.1
    W.C

--- Assets/01_Scripts/Shark.cs
+++ Assets/01_Scripts/Shark.cs
@@ -1,14 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class Shark : Distract
 {
-    void OnTriggerEnter(Collider other)
+    public float jumpForce = 5.0f; 
+    public Kid kid;
+
+    private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Kid"))
+        if (other.CompareTag("Kid") && other.GetComponent<Kid>().target)
         {
-            Destroy(gameObject);
+            Rigidbody kidRigidbody = other.GetComponent < Rigidbody();
+
+            if (kidRigidbody != null)
+            {
+                // Aplica una fuerza hacia arriba para simular el salto.
+                kidRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            }
         }
     }
 }
